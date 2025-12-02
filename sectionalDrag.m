ca681d4b87c9152b9ceb912c45d22b3da08fd314
jakewitte577 <jake.witte@colorado.edu>
2025-12-02 15:08:50 -0700

    Add files via upload
    Lines of code to calculate sectional drag coefficients, based on experimental data

---
+++ sectionalDrag.m
@@ -0,0 +1,12 @@
+%% Code to calculate sectional drag coefficient
+N = 20;
+cl_exp = [-0.8 -0.4 0 0.4 0.8];
+cd_exp = [0.018 0.011 0.01 0.0115 0.018];
+
+[p,S] = polyfit(cl_exp,cd_exp,4);
+
+slope = 0.1;
+alpha = linspace(-5,5,N);
+cl = slope * alpha;
+
+cd = polyval(p, cl);
