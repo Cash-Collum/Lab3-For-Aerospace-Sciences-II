670580ae6096cf0c4fb60b760c2e89209071f1cc
joos1667 <joos1667@colorado.edu>
2025-12-02 13:18:54 -0700

    Create intergral_CD_0.m

---
+++ intergral_CD_0.m
@@ -0,0 +1,18 @@
+function [CD_0] = intergral_CD_0(cd, c_root, c_tip, b)
+b1 = -b/2;
+b2 = b/2;
+
+c1 = @(y) c_root - ((c_root - c_tip)/0.5*b)*y;
+integrand1 = @(y) cd(y) * c1(y);
+
+
+c2 = @(y) c_root + ((c_root - c_tip)/0.5*b)*y;
+integrand2 = @(y) cd(y) * c2(y);
+
+CD_01 = integral(integrand1, b1, 0);
+CD_02 = integral(integrand2, 0, b2);
+
+CD_0 = CD_01 + CD_02;
+
+end
+
